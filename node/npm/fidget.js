de6ffb64c4573a1a755bf0e28c9ed164d57d829c
2003devansh <100955479+2003devansh@users.noreply.github.com>
2024-02-07 17:33:57 +0530

    commit

--- node/npm/fidget.js
+++ node/npm/fidget.js
@@ -1,5 +1,5 @@
-var figlet = require("fidlet");
-figlet("hello devansh",function(err,data){
+var figlet = require("figlet");
+figlet("node",function(err,data){
     if(err){
         console.log("error occur");
         console.dir(err);
