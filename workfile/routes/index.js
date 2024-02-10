0a6a167979affd66d13c1c9a44c103315164ad42
2003devansh <100955479+2003devansh@users.noreply.github.com>
2024-02-10 15:54:15 +0530

    h

--- workfile/routes/index.js
+++ workfile/routes/index.js
@@ -1,9 +1,48 @@
 var express = require('express');
 var router = express.Router();
+const userModel = require("./users");
 
 /* GET home page. */
 router.get("/",function(req,res){
   res.render("index")
 })
+
+//db ke collection asynchronus hote hai toh uskio phele run karwaneke liye/
+// hum async await ka use karte hai
+
+router.get("/create",async function(req,res){
+const createuser =  await userModel.create({
+  username : "devansh",
+  age: 25,
+  name:"faujdar"
+ });
+
+
+ res.send(createuser)
+
+})
+
+
+
+
+router.get("/alluser",async function(req,res){
+  let alluser = await userModel.find();
+  res.send(alluser)
+ })
+
+ 
+router.get("/finduser",async function(req,res){
+  let find1 = await userModel.findOne({username: "devansh"});
+  res.send(find1)
+ })
+
+ router.get("/delete", async function(req,res){
+   let deleteuser = await userModel.findOneAndDelete({
+  username : "devansh"
+   })
+   res.send(deleteuser)
+
+ })
+ 
 
 module.exports = router;
--- workfile/routes/users.js
+++ workfile/routes/users.js
@@ -1,3 +1,17 @@
 const mongoose = require("mongoose");
 
 mongoose.connect("mongodb://127.0.0.1:27017/practice")
+// yeh database create karta hai 
+
+// yeh document ka data hai 
+const userschema = mongoose.Schema({
+    username : String,
+    name: String,
+    age : Number,
+})
+
+//yrh collection banat hai
+
+module.exports= mongoose.model("user",userschema);
+
+//practice db mai "user" naam se ek db ban jayega
