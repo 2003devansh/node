41c4681038a11628c7241ab62165d630adda0c8a
2003devansh <100955479+2003devansh@users.noreply.github.com>
2024-02-11 19:29:41 +0530

    f

---
+++ endgame/routes/users.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/testingendgame");
+
+mongoose.Schema({
+  username: String,
+  nickname: String,
+  description: String,
+  categories : {
+    type : Array,
+    default: []
+  },
+  datecreated : {
+    type: Date,
+    default : Date.now()
+  }
+
+})
+
+mongoose.modelNames("user",userSchema);
